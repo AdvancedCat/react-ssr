f84d282a8ff615b5dc97b7fe6dc95009e5f1edba
Flame <xinhong@anjuke.com>
2018-03-13 17:17:44 +0800

    feat:重新安排webpack

---
+++ webpack/webpack.base.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  devtool: "cheap-module-eval-source-map",
+  entry: path.join(__dirname, '..', '/react/app.js'),
+  output: {
+    // 输出到public目录下
+    path: path.join(__dirname, "..", "public/dist"),
+    filename: "bundle.js",
+    publicPath: "/dist/"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: "babel-loader",
+        exclude: [path.resolve(__dirname, "..", "/node_modules")]
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {}
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  externals: {
+    zepto: "Zepto",
+    react: "React",
+    "react-dom": "ReactDOM"
+  }
+};
